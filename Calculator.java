d14c2313fc3447864cf0b74db6d83ab79501db37
grape45 <grape45@dgu.edu>
2018-03-20 01:52:03 +0900

    add Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static void main(String...args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String...args) {
+        /*
+            print Hello, World!
+         */
+        System.out.println("Hello, World!");
+    }
+}
